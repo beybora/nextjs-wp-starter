77042e8b52f56bbbeea7e1e34f32f0add099017a
beybora <bora.b.sarikaya@gmail.com>
2025-04-14 21:04:01 +0200

    feat(layout): implement responsive root layout structure

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,15 @@
+import Footer from '@/components/sections/Footer/Footer';
 import '../styles/globals.css';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex flex-col min-h-screen bg-gray-300">
+        <main className="flex-grow">
+          {children}
+        </main>
+        <Footer />
+      </body>
     </html>
   )
 }
