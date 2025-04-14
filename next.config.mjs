d213bcc192a481501b0f4452573f0239a0f4d810
beybora <bora.b.sarikaya@gmail.com>
2025-04-14 21:03:10 +0200

    fix(config): correct image domain configuration in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "nextjs-wp-starter-backend.lndo.site/",
+                port: "",
+                pathname: "/wp-content/uploads/**",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
